8c50a848aacfc495ed835638d820db5fadbe12aa
ahmeddoban <ahmedzoro323@gmail.com>
2022-08-22 10:58:22 +0200

    features and home section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*Add active class to menu icon */
 let menu_Icon = document.querySelector(".menue_icon");
 let menu = document.querySelector(".links ul");
 menu_Icon.addEventListener("click", (ele) => {
@@ -9,3 +10,30 @@ menu_Icon.addEventListener("click", (ele) => {
     menu.style.display = "block";
   }
 });
+
+/*Add active class to menu */
+let lis = document.querySelectorAll(".links ul li a");
+lis.forEach((ele) => {
+  ele.addEventListener("click", (ele) => {
+    lis.forEach((ele) => {
+      ele.classList.remove("active");
+    });
+    ele.currentTarget.classList.add("active");
+  });
+});
+/*back to the top btn */
+let ScrollToTop = document.querySelector(".back-to-top");
+window.onscroll = function () {
+  if (window.scrollY >= 150) {
+    ScrollToTop.style.cssText = "display:block !important";
+  } else {
+    ScrollToTop.style.cssText = "display:none";
+  }
+};
+ScrollToTop.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+};
