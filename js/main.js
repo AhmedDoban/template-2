9b2c99bad5e46731d42abfd7f0e473fbaccb60b2
ahmeddoban <ahmedzoro323@gmail.com>
2022-08-22 09:38:19 +0200

    update   menu icon animation

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
-let menu_Icon = document.querySelector(".menu");
+let menu_Icon = document.querySelector(".menue_icon");
 let menu = document.querySelector(".links ul");
 menu_Icon.addEventListener("click", (ele) => {
-  if (menu_Icon.classList.contains("fa-bars")) {
-    menu_Icon.classList.replace("fa-bars", "fa-xmark");
-    menu_Icon.style.cssText = "color:red ; font-size:35px";
-    menu.style.display = "block";
-  } else {
-    menu_Icon.classList.replace("fa-xmark", "fa-bars");
-    menu_Icon.style.cssText = "color: var(--main-color);";
+  if (menu_Icon.classList.contains("active")) {
+    menu_Icon.classList.remove("active");
     menu.style.display = "none";
+  } else {
+    menu_Icon.classList.add("active");
+    menu.style.display = "block";
   }
 });
-
